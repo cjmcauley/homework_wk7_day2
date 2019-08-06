3842860ef5ab7aa78d362ed6001ef5901b450582
cjmcauley <christopher.mcauley@gmail.com>
2019-08-06 21:05:12 +0100

    updated

--- src/app.js
+++ src/app.js
@@ -3,9 +3,25 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => { new Vue({
   el: "#app",
   data: {
-    currencyData: []
+    currencyData: [],
+    selectedCurrency: null,
+    calculateBase: null
   },
-
+  computed: {
+    calculateBase: function(amountbase){
+      amountbase * 10;
+    }
+  },
+  mounted(){
+    this.getCurrencies()
+  },
+  methods: {
+    getCurrencies: function (){
+      fetch("https://api.exchangeratesapi.io/latest")
+      .then(result => result.json())
+      .then(currencies => this.currencyData = currencies.rates)
+    }
+  }
 })
 
 })
